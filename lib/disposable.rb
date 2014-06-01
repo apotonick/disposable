3ff287fd7cafcd9305dc9171ad43d8a4a98dbd2c
Nick Sutterer <apotonick@gmail.com>
2014-06-01 10:06:53 +1000

    remove Facade, it will eventually be available in disposable-facade.

--- lib/disposable.rb
+++ lib/disposable.rb
@@ -5,8 +5,6 @@ module Disposable
   autoload :Composition,  'disposable/composition'
 end
 
-require "disposable/facade"
-
-if defined?(ActiveRecord)
-  require 'disposable/facade/active_record'
-end
+# if defined?(ActiveRecord)
+#   require 'disposable/facade/active_record'
+# end
--- lib/disposable/facade/active_record.rb
+++
@@ -1,8 +0,0 @@
-class Disposable::Facade
-  module ActiveRecord
-    def is_a?(klass)
-      # DISCUSS: should we use facade_options here for the class?
-      klass == __getobj__.class or super
-    end
-  end
-end
