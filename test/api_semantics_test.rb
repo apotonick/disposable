faa22d6d56869079afdca0658e3df4bd63d0f682
Nick Sutterer <apotonick@gmail.com>
2015-04-19 18:33:20 +0200

    refactor semantics into Semantic::Add, etc. make semantics stackable. this allows removing the default "add" behaviour and more. pretty cool.

--- test/api_semantics_test.rb
+++ test/api_semantics_test.rb
@@ -9,9 +9,49 @@ end
 
 module Representable
   class Semantics
+    class Semantic
+    end
+
+    class SkipExisting < Semantic
+      def self.call(model, fragment, index, options)
+        return unless model.songs.collect { |s| s.id.to_s }.include?(fragment["id"].to_s) #and fragment["_action"] != "remove"
+
+        puts "¬¬¬¬¬¬¬ skipping #{fragment}"
+        return Skip.new(fragment)
+      end
+    end
+
+    class Add < Semantic # old representable behavior.
+      def self.call(model, fragment, index, options)
+        binding = options.binding.clone
+        binding.instance_variable_get(:@definition).delete!(:instance) # FIXME: sucks!
+        Representable::Deserializer.new(binding).(fragment, options.user_options) # is that for :update?
+
+        #Model::Song.new # TODO: Original behaviour with :class, :extend, and so on.
+      end
+    end
+
+    class UpdateExisting < Semantic
+      def self.call(model, fragment, index, options)
+        puts "yooo #{model.songs.inspect}"
+        return unless res= model.songs.find { |s| s.id.to_s == fragment["id"].to_s }
+
+          # update existing, but somehow mark it so it does not get added, again.
+
+          return Update.new(res)
+      end
+    end
+
+
     class Skip < OpenStruct
     end
     class Remove < Skip
+      def self.call(model, fragment, index, options)
+        return unless fragment["_action"] == "remove" # TODO: check if feature enabled.
+
+        Remove.new(fragment)
+      end
+
     end
 
     require 'delegate'
@@ -21,40 +61,22 @@ module Representable
     # Per parsed collection item, mark the to-be-populated model for removal, skipping or adding.
     # This code is called right before #from_format is called on the model.
     # Semantical behavior is inferred from the fragment making this code document- and format-specific.
+
+    # remove: unlink from association
+    # skip_existing
+    # update_existing
+    # [add]
+
+    # default behavior: - add_new
+
     class Instance
       include Uber::Callable
 
       def call(model, fragment, index, options)
         semantics = options.binding[:semantics]
 
-        if fragment["_action"] == "remove" # TODO: check if feature enabled.
-          Remove.new(fragment) # the deserializer will assign {id: 2, title: "Sunlit Nights"}.
-        else
-          # the if is an optional feature!
-          if semantics.include?(:skip_existing)
-            if model.songs.collect { |s| s.id.to_s }.include?(fragment["id"].to_s) #and fragment["_action"] != "remove"
-
-              puts "¬¬¬¬¬¬¬ skipping #{fragment}"
-              return Skip.new(fragment)
-
-            else
-              Model::Song.new
-            end
-
-          elsif semantics.include?(:update_existing)
-            puts "yooo #{model.songs.inspect}"
-            if res= model.songs.find { |s| s.id.to_s == fragment["id"].to_s }
-              puts "$$$ #{res}"
-
-              # update existing, but somehow mark it so it does not get added, again.
-
-              return Update.new(res)
-            else
-              Model::Song.new
-            end
-          else
-            Model::Song.new
-          end
+        semantics.each do |semantic|
+          res = semantic.(model, fragment, index, options) and return res
         end
       end
     end
@@ -86,12 +108,15 @@ puts "Setter: #{values.inspect}"
   end
 end
 
+# TODO: test remove when :remove semantic is not set.
+
 class AlbumDecorator < Representable::Decorator
   include Representable::Hash
 
   collection :songs,
 
-    semantics: [:skip_existing],
+    # semantics: [:skip_existing, :add, :remove],
+    semantics: [Representable::Semantics::Remove, Representable::Semantics::SkipExisting, Representable::Semantics::Add],
 
     instance: Representable::Semantics::Instance.new,
 
@@ -100,7 +125,10 @@ class AlbumDecorator < Representable::Decorator
     #   puts "sss #{fragment.inspect}... #{songs.collect { |s| s.id.to_s }.inspect}"
     #   songs.collect { |s| s.id.to_s }.include?(fragment["id"].to_s) }, # read-only existing.
 
-    setter: Representable::Semantics::Setter.new do # add new to existing collection.
+    setter: Representable::Semantics::Setter.new,
+
+
+    class: Model::Song do # add new to existing collection.
 
       # only add new songs
       property :title
@@ -110,7 +138,7 @@ end
 
 
 class ApiSemantics < MiniTest::Spec
-  it do
+  it "xxx" do
     album = Model::Album.new(1, "And So I Watch You From Afar", [Model::Song.new(2, "Solidarity"), Model::Song.new(0, "Tale That Wasn't Right")])
 
     decorator = AlbumDecorator.new(album)
@@ -134,11 +162,12 @@ class ApiSemanticsWithUpdate < MiniTest::Spec
 
     collection :songs,
 
-      semantics: [:update_existing],
+      semantics: [Representable::Semantics::Remove, Representable::Semantics::UpdateExisting, Representable::Semantics::Add],
 
       instance: Representable::Semantics::Instance.new,
 
         pass_options: true,
+        class: Model::Song,
       # skip_parse: lambda { |fragment, *args|
       #   puts "sss #{fragment.inspect}... #{songs.collect { |s| s.id.to_s }.inspect}"
       #   songs.collect { |s| s.id.to_s }.include?(fragment["id"].to_s) }, # read-only existing.
