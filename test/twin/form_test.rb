255470f06b728615ce36e58a29cf4aebf9951430
Nick Sutterer <apotonick@gmail.com>
2015-05-07 15:26:42 +1000

    introduce Schema to convert a full-blown representer back to its structure. WIP.

--- test/twin/form_test.rb
+++ test/twin/form_test.rb
@@ -8,33 +8,6 @@ class FormTest < MiniTest::Spec
   end
 
 
-
-  # TODO: this needs tests and should probably go to Representable. we can move tests from Reform for that.
-  class Converter
-    def self.from(source_class, options) # TODO: can we re-use this for all the decorator logic in #validate, etc?
-      representer = Class.new(options[:superclass])
-      representer.send :include, *options[:include]
-
-      source_class.representable_attrs.each do |dfn|
-        representer.property(dfn.name, dfn.instance_variable_get(:@options)) unless dfn[:extend]
-
-        if twin = dfn[:twin]
-          twin = twin.evaluate(nil)
-
-          dfn_options = dfn.instance_variable_get(:@options).merge(extend: from(twin.object_representer_class, options))
-
-          if dfn_options[:deserializer]
-            dfn_options.merge!(dfn_options[:deserializer])
-          end
-
-          representer.property(dfn.name, dfn_options)
-        end
-      end
-
-      representer
-    end
-  end
-
   module Validate # TODO: use from reform.
     def validate(params)
       # 1. create representer
