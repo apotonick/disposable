f9256987be32108921dca26c639e61a6a75a6529
Nick Sutterer <apotonick@gmail.com>
2015-05-10 17:38:15 +1000

    finish changed_test for now. we don't support #changed? without property name, yet.

--- test/twin/changed_test.rb
+++ test/twin/changed_test.rb
@@ -32,19 +32,16 @@ class ChangedWithSetupTest < MiniTest::Spec
     end
   end
 
-  # setup: changed? is always false
+
   let (:song)     { Model::Song.new("Broken") }
   let (:composer) { Model::Artist.new(2) }
   let (:song_with_composer) { Model::Song.new("Broken", 246, composer) }
   let (:artist)   { Model::Artist.new("Randy") }
   let (:album)    { Model::Album.new("The Rest Is Silence", [song, song_with_composer], artist) }
+  let (:twin)     { Twin::Album.new(album) }
 
+  # setup: changed? is always false
   it do
-    twin = Twin::Album.new(album)
-
-
-    twin.songs[0].changed.must_equal( {})
-
     twin.changed?(:name).must_equal false
     twin.changed?.must_equal false
 
@@ -58,6 +55,35 @@ class ChangedWithSetupTest < MiniTest::Spec
 
     twin.artist.changed?(:name).must_equal false
     twin.artist.changed?.must_equal false
+  end
+
+  # only when a property is assigned, it's changed.
+  it do
+    twin.name= "Out Of Bounds"
+    twin.songs[0].title= "I Have Seen"
+    twin.songs[1].title= "In A Rhyme"
+    twin.songs[1].composer.name= "Ingemar Jansson & Mikael Danielsson"
+    twin.artist.name = "No Fun At All"
+
+    twin.changed?(:name).must_equal true
+    # twin.changed?.must_equal true
+
+    # twin.songs[0].changed?.must_equal true
+    twin.songs[0].changed?(:title).must_equal true
+    # twin.songs[1].changed?.must_equal true
+    twin.songs[1].changed?(:title).must_equal true
+
+    twin.songs[1].composer.changed?(:name).must_equal true
+    # twin.songs[1].composer.changed?.must_equal true
+
+    twin.artist.changed?(:name).must_equal true
+    # twin.artist.changed?.must_equal true
+  end
+
+  # nested changes should propagate up.
+  it do
+    twin.songs[1].composer.name = "Nofx"
+
   end
 
 
