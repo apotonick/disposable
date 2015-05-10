47a673791f21c70f9ec4564c25545054b2d70519
Nick Sutterer <apotonick@gmail.com>
2015-05-11 09:10:49 +1000

    cleanup.

--- lib/disposable/twin/changed.rb
+++ lib/disposable/twin/changed.rb
@@ -1,19 +1,15 @@
 module Disposable::Twin::Changed
   def changed?(name=nil)
-    # puts "+++#{model.class}  #{name}: #{changed.inspect} (in #{model.class})"
     !! changed[name.to_s]
   end
 
   def changed
-    # puts "++%%%+#{model.class} "
     @changed ||= {}
   end
 
 private
   def initialize(model, *args)
     super # Initialize, Setup
-
-    puts "#{model.class} Changed#initialize"
     @changed = {}
   end
 
@@ -21,7 +17,6 @@ private
     old_value = send(name) # FIXME: what about the private_name stuff?
 
     super.tap do
-      # puts "comparing #{old_value} and #{value}"
       changed[name.to_s] = old_value != value
     end
   end
