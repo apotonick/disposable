4417fab474b7faa3ffa9dccaf12f0ddba560272e
Nick Sutterer <apotonick@gmail.com>
2015-05-11 11:42:16 +1000

    more tests for #sync.

--- test/twin/sync_test.rb
+++ test/twin/sync_test.rb
@@ -2,75 +2,95 @@ require "test_helper"
 
 class TwinSyncTest < MiniTest::Spec
   module Model
-    Song  = Struct.new(:id, :title, :album, :composer)
-    Album = Struct.new(:id, :name, :songs, :artist)
-    Artist = Struct.new(:id)
+    Song  = Struct.new(:title, :composer)
+    Album = Struct.new(:name, :songs, :artist)
+    Artist = Struct.new(:name)
   end
 
 
   module Twin
     class Album < Disposable::Twin
-      property :id
+      feature Setup
+      feature Sync
+
       property :name
-      collection :songs, :twin => lambda { |*| Song }
-      property :artist, twin: lambda { |*| Artist }
 
-      extend Representer
-      include Setup
-      include Sync
-    end
+      collection :songs do
+        property :title
 
-    class Song < Disposable::Twin
-      property :id
-      property :composer, :twin => lambda { |*| Artist }
+        property :composer do
+          property :name
+        end
+      end
 
-      extend Representer
-      include Setup
-      include Sync
-    end
-
-    class Artist < Disposable::Twin
-      property :id
-
-      extend Representer
-      include Setup
-      include Sync
+      property :artist do
+        property :name
+      end
     end
   end
 
 
   let (:song) { Model::Song.new() }
   let (:composer) { Model::Artist.new(nil) }
-  let (:song_with_composer) { Model::Song.new(nil, nil, nil, composer) }
+  let (:song_with_composer) { Model::Song.new(nil, composer) }
   let (:artist) { Model::Artist.new(nil) }
 
+  # with populated model.
   describe "#sync" do
-    let (:album) { Model::Album.new(nil, nil, [song, song_with_composer], artist) }
+    let (:album) { Model::Album.new(nil, [song, song_with_composer], artist) }
 
     it do
       twin = Twin::Album.new(album)
 
       # this usually happens in Contract::Validate or in from_* in a representer
       twin.name = "Live And Dangerous"
-      twin.songs[0].id = 1
-      twin.songs[1].id = 2
-      twin.songs[1].composer.id = 3
-      twin.artist.id = "Thin Lizzy"
+      twin.songs[0].title = "Southbound"
+      twin.songs[1].title = "The Boys Are Back In Town"
+      twin.songs[1].composer.name = "Lynott"
+      twin.artist.name = "Thin Lizzy"
 
       # not written to model, yet.
       album.name.must_equal nil
-      album.songs[0].id.must_equal nil
-      album.songs[1].id.must_equal nil
-      album.songs[1].composer.id.must_equal nil
-      album.artist.id.must_equal nil
+      album.songs[0].title.must_equal nil
+      album.songs[1].title.must_equal nil
+      album.songs[1].composer.name.must_equal nil
+      album.artist.name.must_equal nil
 
       twin.sync
 
       album.name.must_equal "Live And Dangerous"
-      album.songs[0].id.must_equal 1
-      album.songs[1].id.must_equal 2
-      album.songs[1].composer.id.must_equal 3
-      album.artist.id.must_equal "Thin Lizzy"
+      album.songs[0].title.must_equal "Southbound"
+      album.songs[1].title.must_equal "The Boys Are Back In Town"
+      album.songs[1].composer.name.must_equal "Lynott"
+      album.artist.name.must_equal "Thin Lizzy"
+    end
+
+    # with empty, not populated model.
+    it do
+      album = Model::Album.new(nil, [])
+      twin  = Twin::Album.new(album)
+
+      # this usually happens in Contract::Validate or in from_* in a representer
+      twin.name = "Live And Dangerous"
+
+      twin.songs.insert(0, song)
+      twin.songs.insert(1, song_with_composer)
+
+      twin.songs[0].title = "Southbound"
+      twin.songs[1].title = "The Boys Are Back In Town"
+      twin.songs[1].composer.name = "Lynott"
+
+      # not written to model, yet.
+      album.name.must_equal nil
+      album.songs.must_equal []
+      album.artist.must_equal nil
+
+      twin.sync # this assigns a new collection via #songs=.
+
+      album.name.must_equal "Live And Dangerous"
+      album.songs[0].title.must_equal "Southbound"
+      album.songs[1].title.must_equal "The Boys Are Back In Town"
+      album.songs[1].composer.name.must_equal "Lynott"
     end
   end
 end
