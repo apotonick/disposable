93332759bf77a7c694c7efd53b212204ccd05ce5
Nick Sutterer <apotonick@gmail.com>
2015-05-23 21:05:20 +1000

    add inherit_test.

---
+++ test/twin/inherit_test.rb
@@ -0,0 +1,42 @@
+require "test_helper"
+
+class InheritTest < MiniTest::Spec
+  module Model
+    Song  = Struct.new(:title, :album)
+    Album = Struct.new(:name, :songs, :artist)
+    Artist = Struct.new(:name)
+  end
+
+  module Twin
+    class Album < Disposable::Twin
+      feature Setup
+
+      property :artist do
+        property :name
+
+        def artist_id
+          1
+        end
+      end
+    end
+
+    class EmptyCompilation < Album
+    end
+
+    class Compilation < Album
+      property :artist, inherit: true do
+
+      end
+    end
+  end
+
+  let (:album) { Model::Album.new("In The Meantime And Inbetween Time", [], Model::Artist.new) }
+
+  it { Twin::Album.new(album).artist.artist_id.must_equal 1 }
+
+  # inherit inline twins when not overriding.
+  it { Twin::EmptyCompilation.new(album).artist.artist_id.must_equal 1 }
+
+  # inherit inline twins when overriding.
+  it { Twin::Compilation.new(album).artist.artist_id.must_equal 1 }
+end
