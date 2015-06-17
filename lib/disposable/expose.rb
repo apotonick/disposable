b60285483f4e914be6730a0b31ef45e4e2af5110
Nick Sutterer <apotonick@gmail.com>
2015-06-17 10:13:57 +1000

    implement Expose.

--- lib/disposable/expose.rb
+++ lib/disposable/expose.rb
@@ -1,5 +1,22 @@
 module Disposable
   class Expose
+    def self.from(representer)
+      representer.representable_attrs.each do |definition|
+        process_definition!(definition)
+      end
+    end
 
+  private
+    def self.process_definition!(definition)
+      public_name  = definition.name
+      private_name = definition[:private_name] || public_name
+
+      define_method("#{public_name}")  { @model.send("#{private_name}") }
+      define_method("#{public_name}=") { |*args| @model.send("#{private_name}=", *args) }
+    end
+
+    def initialize(model)
+      @model = model
+    end
   end
 end
--- test/expose_test.rb
+++ test/expose_test.rb
@@ -2,61 +2,100 @@ require "test_helper"
 
 class ExposeTest < MiniTest::Spec
   module Model
-    Band  = Struct.new(:id, :title)
-    Album = Struct.new(:id, :name, :album)
+    Album = Struct.new(:id, :name)
   end
 
   module Twin
     class Album < Disposable::Twin
-      property :id,                 on: :album
-      property :name,               on: :album
-      property :band_id, from: :id, on: :band
-      property :title,              on: :band
-    end
-
-    class AlbumExpose < Disposable::Expose
-
+      property :id
+      property :title, from: :name
     end
   end
 
-  let (:band) { Model::Band.new(1, "Frenzal Rhomb") }
-  let (:album) { Model::Album.new(2, "Dick Sandwich", "For the Term of Their Unnatural Lives") }
-  subject { Twin::Album.new(:album => album, :band => band) }
+  class AlbumExpose < Disposable::Expose
+    from Twin::Album.representer_class
+  end
 
-  # describe "readers" do
-  #   it { subject.id.must_equal 2 }
-  #   it { subject.band_id.must_equal 1 }
-  #   it { subject.name.must_equal "Dick Sandwich" }
-  #   it { subject.album.must_equal "For the Term of Their Unnatural Lives" }
-  #   it { subject.title.must_equal "Frenzal Rhomb" }
-  # end
+  let (:album) { Model::Album.new(1, "Dick Sandwich") }
+  subject { AlbumExpose.new(album) }
+
+  describe "readers" do
+    it { subject.id.must_equal 1 }
+    it { subject.title.must_equal "Dick Sandwich" }
+  end
 
 
-  # describe "writers" do
-  #   before do
-  #     subject.id = 3
-  #     subject.band_id = 4
-  #     subject.name = "Eclipse"
-  #     subject.title = "Yngwie J. Malmsteen"
-  #     subject.album = "Best Of"
-  #   end
+  describe "writers" do
+    it do
+      subject.id = 3
+      subject.title = "Eclipse"
 
-  #   it { subject.id.must_equal 3 }
-  #   it { album.id.must_equal   3 }
-  #   it { subject.band_id.must_equal 4 }
-  #   it { band.id.must_equal 4 }
-  #   it { subject.name.must_equal "Eclipse" }
-  #   it { subject.title.must_equal "Yngwie J. Malmsteen" }
-  #   it { album.name.must_equal "Eclipse" }
-  #   it { band.title.must_equal "Yngwie J. Malmsteen" }
-  #   it { subject.album.must_equal "Best Of" }
-  #   it { album.album.must_equal "Best Of" }
-  # end
-  # # it { subject.save }
-
-  # it "raises when non-mapped property" do
-  #   assert_raises NoMethodError do
-  #     subject.raise_an_exception
-  #   end
-  # end
+      subject.id.must_equal 3
+      subject.title.must_equal "Eclipse"
+      album.id.must_equal 3
+      album.name.must_equal "Eclipse"
+    end
+  end
 end
+
+
+# class ExposeCompositionTest < MiniTest::Spec
+#   module Model
+#     Band  = Struct.new(:id, :title)
+#     Album = Struct.new(:id, :name, :album)
+#   end
+
+#   module Twin
+#     class Album < Disposable::Twin
+#       property :id,                 on: :album
+#       property :name,               on: :album
+#       property :band_id, from: :id, on: :band
+#       property :title,              on: :band
+#     end
+
+#     class AlbumExpose < Disposable::Expose
+#       from Twin::Album.representer_class
+#     end
+#   end
+
+#   let (:band) { Model::Band.new(1, "Frenzal Rhomb") }
+#   let (:album) { Model::Album.new(2, "Dick Sandwich", "For the Term of Their Unnatural Lives") }
+#   subject { Twin::Album.new(:album => album, :band => band) }
+
+#   describe "readers" do
+#     it { subject.id.must_equal 2 }
+#     it { subject.band_id.must_equal 1 }
+#     it { subject.name.must_equal "Dick Sandwich" }
+#     it { subject.album.must_equal "For the Term of Their Unnatural Lives" }
+#     it { subject.title.must_equal "Frenzal Rhomb" }
+#   end
+
+
+#   describe "writers" do
+#     before do
+#       subject.id = 3
+#       subject.band_id = 4
+#       subject.name = "Eclipse"
+#       subject.title = "Yngwie J. Malmsteen"
+#       subject.album = "Best Of"
+#     end
+
+#     it { subject.id.must_equal 3 }
+#     it { album.id.must_equal   3 }
+#     it { subject.band_id.must_equal 4 }
+#     it { band.id.must_equal 4 }
+#     it { subject.name.must_equal "Eclipse" }
+#     it { subject.title.must_equal "Yngwie J. Malmsteen" }
+#     it { album.name.must_equal "Eclipse" }
+#     it { band.title.must_equal "Yngwie J. Malmsteen" }
+#     it { subject.album.must_equal "Best Of" }
+#     it { album.album.must_equal "Best Of" }
+#   end
+#   # it { subject.save }
+
+#   it "raises when non-mapped property" do
+#     assert_raises NoMethodError do
+#       subject.raise_an_exception
+#     end
+#   end
+# end
