cc6e8b05c0618302fc7a51853e4aaa93cf795e67
John D'Agostino <johnd@jobready.com.au>
2015-09-01 15:34:59 +1000

    this commit adds new setter module allowing for declarative approach to defining custom setters for properties

---
+++ test/twin/setter_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class SetterTest < Minitest::Spec
+  Composer = Struct.new(:name)
+
+  class Twin < Disposable::Twin
+    feature Setter
+
+    property :name, setter: -> (v) { v.sub(/cat/, 'dog') unless v.nil? }
+  end
+
+  it do
+    twin = Twin.new(Composer.new)
+    twin.name = "cat"
+    twin.name.must_equal "dog"
+  end
+end
