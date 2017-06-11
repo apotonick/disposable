ccc256cf0a4367e5046ffde90c8dc17b71b135db
Nick Sutterer <apotonick@gmail.com>
2017-06-11 15:36:19 +0200

    start testing struct features in separate test files.

---
+++ test/twin/struct/coercion_test.rb
@@ -0,0 +1,36 @@
+require "test_helper"
+require "disposable/twin/coercion"
+require "disposable/twin/property/struct"
+
+class StructCoercionTest < Minitest::Spec
+  ExpenseModel = Struct.new(:content)
+
+  class Expense < Disposable::Twin
+    feature Property::Struct
+    feature Coercion
+
+    property :content do
+      property :amount, type: Types::Form::Float
+    end
+
+    unnest :amount, from: :content
+  end
+
+
+  it do
+    twin = Expense.new( ExpenseModel.new({}) )
+
+    #- direct access, without unnest
+    twin.content.amount.must_equal nil
+    twin.content.amount = "1.8"
+    twin.content.amount.must_equal 1.8
+  end
+
+  it "via unnest" do
+    twin = Expense.new( ExpenseModel.new({}) )
+
+    twin.amount.must_equal nil
+    twin.amount = "1.8"
+    twin.amount.must_equal 1.8
+  end
+end
