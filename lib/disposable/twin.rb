a5338bd54ce6b1887554c36bf52a95ef6d6a96f4
Nick Sutterer <apotonick@gmail.com>
2021-11-20 11:43:18 +0100

    Revert "allow kw args in `#property`."
    This reverts commit aec0870bfe28f993da3129fa25a3b5d612e2e463.

--- lib/disposable/twin.rb
+++ lib/disposable/twin.rb
@@ -1,3 +1,4 @@
+# require "uber/inheritable_attr"
 require "declarative/schema"
 require "representable/decorator"
 
@@ -52,8 +53,8 @@ module Disposable
         end
       end
 
-      def collection(name, **options, &block)
-        property(name, **options.merge(collection: true), &block)
+      def collection(name, options={}, &block)
+        property(name, options.merge(collection: true), &block)
       end
 
       require "disposable/twin/property/unnest"
