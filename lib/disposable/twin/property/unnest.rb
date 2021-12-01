684a8929c495123a3e7537d2e0be1051342a007f
Nick Sutterer <apotonick@gmail.com>
2021-12-01 12:59:07 +0100

    remove `Forwardable` from `Unnest` and refrain from including it into the `Twin` class. This may overwrite the same-named `ActiveSupport::delegate` method and breaks things. Instead, do manual delegation and plan for 2023 to allow using `Forwardable.delegate(target, what, where)`. Fixes https://github.com/trailblazer/reform/issues/534

--- lib/disposable/twin/property/unnest.rb
+++ lib/disposable/twin/property/unnest.rb
@@ -1,12 +1,6 @@
-require "forwardable"
-
 module Disposable::Twin::Property
   module Unnest
-    extend Forwardable
     # TODO: test that nested properties options are "unnested", too, e.g. populator.
-    def self.included(includer)
-      includer.send(:include, Forwardable)
-    end
 
     def unnest(name, options)
       from = options.delete(:from)
@@ -16,7 +10,15 @@ module Disposable::Twin::Property
       options = options.merge(virtual: true, _inherited: true, private_name: nil)
 
       property(name, options)
-      def_delegators from, name, "#{name}="
+      # def_delegators from, name, "#{name}=" # FIXME: this overwrites ActiveSupport#delegate in some cases.
+
+      define_method name do
+        send(from).send(name) # TODO: fix Forwardable in Ruby.
+      end
+
+      define_method "#{name}=" do |value|
+        send(from).send("#{name}=", value)
+      end
     end
   end
 end
